43bb3bca0a56fbb71df481f5b482b9147bb3312a
sanafancy <xutingting197@gmail.com>
2024-11-12 18:14:20 +0100

    Actualización de la clase Usuario

--- src/main/java/com/example/demo/dominio/entidades/Usuario.java
+++ src/main/java/com/example/demo/dominio/entidades/Usuario.java
@@ -1,4 +1,41 @@
 package com.example.demo.dominio.entidades;
 
-public class Usuario {
-}
+import jakarta.persistence.*;
+/*
+@Entity
+//@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idUsuario;
+    @Column
+    private String pass;
+
+    public Usuario(String pass) {
+        this.pass=pass;
+    }
+
+    public String getIdUsuario() {
+         return idUsuario;
+    }
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    public String getPass() {
+        return pass;
+    }
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    //otro metodo
+    public boolean autenticar(String password) {
+        return this.pass.equals(password);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Greeting [idUsuario=%s, password=%s]", idUsuario, pass);
+    }
+}*/
